d81561102ab30caac68e80e1685ce3f4a1ed9908
Yahyo <yahyobafoyev@gmail.com>
2025-04-29 18:42:10 +0500

    start

---
+++ src/main/java/com/company/SuppyChainLiteApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppyChainLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuppyChainLiteApplication.class, args);
+    }
+
+}
